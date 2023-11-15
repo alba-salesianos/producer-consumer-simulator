e219cc80d5a844b3ed979be3b73e4041dfaf98c1
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-11-15 13:48:46 +0000

    refactor: deleted unnecessary imports

--- src/net/salesianos/Main.java
+++ src/net/salesianos/Main.java
@@ -3,7 +3,6 @@ package net.salesianos;
 import net.salesianos.entities.Customer;
 import net.salesianos.entities.Farmer;
 import net.salesianos.storage.Restaurant;
-import net.salesianos.utils.Utils;
 
 public class Main {
     public static void main(String[] args) {
