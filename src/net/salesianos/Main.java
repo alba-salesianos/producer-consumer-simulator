871bd930fc8739ae6e9e37c1d895857ba308aada
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-11-15 13:20:45 +0000

    test: changed parameters of the program

--- src/net/salesianos/Main.java
+++ src/net/salesianos/Main.java
@@ -9,19 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant(5);
         Farmer newFarmer = new Farmer(10, "Yamada", restaurant);
-        Farmer newFarmer2 = new Farmer(10, "Inoo", restaurant);
-        Farmer newFarmer3 = new Farmer(10,"Hikaru", restaurant);
 
+        Customer customer = new Customer(10, "Bangchan", restaurant);
 
-        Customer customer = new Customer(15, "Bangchan", restaurant);
-        Customer customer2 = new Customer(15, "Changbin", restaurant);
 
         newFarmer.start();
-        newFarmer2.start();
-        newFarmer3.start();
         customer.start();
-        customer2.start();
-
 
     }
 }
