f9c1723af9c7daf7eb1bc78cfe16a0743f0010de
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-11-15 13:44:17 +0000

    test: changed parameters of the program for test 3

--- src/net/salesianos/Main.java
+++ src/net/salesianos/Main.java
@@ -8,20 +8,14 @@ import net.salesianos.utils.Utils;
 public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant(5);
+
         Farmer newFarmer = new Farmer(10, "Yamada", restaurant);
-        Farmer newFarmer2 = new Farmer(10, "Inoo", restaurant);
-        Farmer newFarmer3 = new Farmer(10,"Hikaru", restaurant);
+        Customer customer = new Customer(10, "Bangchan", restaurant);
 
-
-        Customer customer = new Customer(15, "Bangchan", restaurant);
-        Customer customer2 = new Customer(15, "Changbin", restaurant);
+        newFarmer.setPriority(Thread.MAX_PRIORITY);
+        customer.setPriority(Thread.MIN_PRIORITY);
 
         newFarmer.start();
-        newFarmer2.start();
-        newFarmer3.start();
         customer.start();
-        customer2.start();
-
-
     }
 }
